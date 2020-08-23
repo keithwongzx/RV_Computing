359c3c40ed0bb9725edb70aa6624ffc88848d990
keithwongzx <60955798+keithwongzx@users.noreply.github.com>
2020-08-23 23:43:45 +0800

    update

--- Tutorial_N02:03/app.py
+++ Tutorial_N02:03/app.py
@@ -3,13 +3,19 @@ from flask import Flask, render_template, url_for, redirect, request
 app = Flask(__name__)
 
 @app.route("/")
-def home:
-    render_template("home.html")
+def home():
+    return render_template("home.html")
 
 @app.route("/about/")
-def about:
-    render_template("about.html")
+def about():
+    return render_template("about.html")
 
-@app.route("/rp_calc/")
-def rp_calc:
-    render_template("rp_calc")
+@app.route("/rp_calc/", methods=["GET", "POST"])
+def rp_calc():
+    if request.method == "GET":
+        return render_template("rp_calc.html")
+    else:
+        return render_template("rp_calc_select.html")
+
+if __name__ == "__main__":
+    app.run(port=5003, debug=True)
